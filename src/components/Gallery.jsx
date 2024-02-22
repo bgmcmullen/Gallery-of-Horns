2c94e8bfc02685b219bde5bcde35fb30751f70a4
Brendan <bgmcmullen@comcast.net>
2024-02-22 15:42:24 -0500

    Display Images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable react/prop-types */
-import HornedBeast from "./HornedBeast"
+import HornedBeast from "./HornedBeast";
+import hornedBestValues from '../assets/data.json';
 
 function Gallery(props){
   return(
-  <>
-  <HornedBeast title={props.imagesArray[0].title} imageUrl={props.imagesArray[0].image_url} description={props.imagesArray[0].description}/>
-  <HornedBeast title={props.imagesArray[1].title} imageUrl={props.imagesArray[1].image_url} description={props.imagesArray[1].description}/>
-  <HornedBeast title={props.imagesArray[2].title} imageUrl={props.imagesArray[2].image_url} description={props.imagesArray[2].description}/>
-  </>
+    <section>
+      {hornedBestValues.map((beast) => <HornedBeast title={beast.title} imageUrl={beast.image_url} description={beast.description}/>)};
+    </section>
   )
 
 }
