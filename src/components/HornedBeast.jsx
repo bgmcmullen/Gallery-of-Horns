a52b436f933e1b68d28e5034515c1edb6d57e524
Brendan <bgmcmullen@comcast.net>
2024-02-22 16:07:14 -0500

    Allow Users to Favorite Individual Beasts

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 function HornedBeast(props){
 
+  let [favorites, setfavorites] = useState(0);
 
+  function addFavorite() {
+    setfavorites(++favorites);
+  }
   return(
   <>
     <h2>{props.title}</h2>
-    <img src={props.imageUrl} alt={props.alt}></img>
+    <img onClick={addFavorite} src={props.imageUrl} alt={props.alt}></img>
+    <div id="favorites">❤️{favorites}</div>
     <p>{props.description}</p>
   </>
   )
